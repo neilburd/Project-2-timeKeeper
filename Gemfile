641dd5a077defd1ae18ba23f83ba379408d2630a
“neilburd” <neilburd@gmail.com>
2017-01-06 14:24:31 -0500

    What is going on with this database

--- Gemfile
+++ Gemfile
@@ -8,9 +8,7 @@ end
 gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
-# Use Puma as the app server
+# Use sqlite3 as the database for Active Record# Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
@@ -20,7 +18,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'pg', '~> 0.18'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170106141322) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170106141322) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_jobs_on_user_id"
+    t.index ["user_id"], name: "index_jobs_on_user_id", using: :btree
   end
 
   create_table "timers", force: :cascade do |t|
@@ -30,7 +33,7 @@ ActiveRecord::Schema.define(version: 20170106141322) do
     t.datetime "updated_at",  null: false
     t.integer  "hours"
     t.integer  "minutes"
-    t.index ["job_id"], name: "index_timers_on_job_id"
+    t.index ["job_id"], name: "index_timers_on_job_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,8 +49,10 @@ ActiveRecord::Schema.define(version: 20170106141322) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "jobs", "users"
+  add_foreign_key "timers", "jobs"
 end
